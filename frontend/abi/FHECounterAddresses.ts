8893b23542fd3e49d780d606c8095006430c3799
MyronHerty <tvpedwxe749694@outlook.com>
2025-11-06 11:08:00 -0800

    chore: update FHECounter contract address on Sepolia

--- frontend/abi/FHECounterAddresses.ts
+++ frontend/abi/FHECounterAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const FHECounterAddresses = { 
-  "11155111": { address: "0x43d364c34883F2ed699291cc5341594c229328A4", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0x696a8f732945675960F67758392B97FB10dC734A", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", chainId: 31337, chainName: "hardhat" },
 };
